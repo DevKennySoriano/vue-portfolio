57aebe879be04540513ec98b67dee45ca45f8011
Kenny Soriano <devkennysoriano@gmail.com>
2026-04-15 00:18:02 +0800

    Adjusted Certificates

--- src/composables/useCertificateCarousel.js
+++ src/composables/useCertificateCarousel.js
@@ -15,7 +15,7 @@ export function useCertificateCarousel() {
       image: '/images/certs/JavaFundamentals.jpg',
     },
     {
-      title: 'JavaScript In Celebratin of Nutrition Month 2019 with the Theme (Kumain ng Wasto at Maging Aktibo...Push Natin To) Given 26th of July 2019',
+      title: 'In Celebration of Nutrition Month 2019 with the Theme (Kumain ng Wasto at Maging Aktibo...Push Natin To) Given 26th of July 2019',
       issuer: 'Mabalacat National Senior High School - Dau, Mabalacat Pampanga',
       date: '2019',
       image: '/images/certs/Tshirt.jpg',
