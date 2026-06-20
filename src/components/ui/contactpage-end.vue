4ced4e24812106e662599fc14aad14573c921a14
DevKennySoriano <devkennysoriano@gmail.com>
2026-06-20 12:05:31 +0800

    removed: temporary

--- src/components/ui/contactpage-end.vue
+++ src/components/ui/contactpage-end.vue
@@ -3,9 +3,9 @@
 
 <template>
   <section class="collab" data-aos="fade-up" data-aos-duration="900" data-aos-once="true">
-    <h2 class="title">Want to hear what people say?</h2>
+    <h2 class="title"></h2>
     <p class="text">
-      Reach out to request references or feedback from mentors and collaborators.
+    
     </p>
   </section>
 </template>
